5e220b982a23c97661d2e69cad22fc4701d7896b
Harsh Sahu <harshsahu7765858311@gmail.com>
2023-08-30 19:31:09 +0530

    created login screen

--- app/src/main/java/com/main/healthcare/healthproject/MainActivity.kt
+++ app/src/main/java/com/main/healthcare/healthproject/MainActivity.kt
@@ -1,80 +1,101 @@
 package com.main.healthcare.healthproject
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.main.healthcare.healthproject.ui.theme.HealthProjectTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.KeyboardType
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity2 : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContent {
-            HealthProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContent{
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                LoginScreen()
             }
+
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp).background(Color.White),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Login screen",
+            style = MaterialTheme.typography.headlineMedium,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally).padding(horizontal = 10.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Email") },
+            placeholder = { Text(text = "Enter your email here")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Password") },
+            placeholder = { Text(text = "Enter your password here")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp).padding(horizontal = 10.dp)
+        ) {
+            Text(text = "Submit")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+@Preview
+fun LoginScreenPreview() {
+    LoginScreen()
 }
-@Composable
-fun CustomWidget(){
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Cyan), ) {
-        Card(modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(), shape = RoundedCornerShape(11.dp), elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = "#Harsh Sahu",modifier = Modifier.padding(10.dp).align(Alignment.CenterHorizontally))
-                    Spacer(modifier = Modifier.padding(horizontal = 10.dp))
-                    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(vertical = 3.dp, horizontal = 10.dp).align(Alignment.CenterHorizontally)) {
-                        Text(text = "Ok",modifier = Modifier.padding(3.dp))
-                    }
-                    Spacer(modifier = Modifier.padding(vertical = 10.dp))
-                }            }
-        }
-    }
 
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HealthProjectTheme {
-        CustomWidget()
-    }
-}
+
